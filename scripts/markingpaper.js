ba6b4b882d3079a9bf3d149d61416e2ad4408565
unknown <dojusara@gmail.com>
2023-07-02 22:49:06 +0900

    이전 커밋 누락 파일

--- scripts/markingpaper.js
+++ scripts/markingpaper.js
@@ -1,40 +1,64 @@
+// 20230628
+// edited 20230701
 
-// 답안 항목들 배열
-var questions = [
-    {
-        bShowAnswerImme: false,
-        bShowCorrectImme: false,
-        options: ["Option 1","Option 2","Option 3","Option 4"], // 현재는 사용 안 함
-        nCorrectAnswer: 0
-    },
-];
+var questions = []; // 답안 항목들 배열
 
 var markingContainer = document.getElementById("markingContainer");
 var userResponses = [];
 
-let bShowAnswer = false;    // 정답 표시
-let bShowCorrect = false;   // 체크한 문항이 정답인가 표시
+var bShowAnswer = false;    // 정답 표시
+var bShowCorrect = false;   // 체크한 문항이 정답인가 표시
 
-//  var nSizeofQuestion = 100;  // 문항 갯수
-
-const controller = new AbortController();
 var btn = document.getElementById('btn202204');
 
-btn.addEventListener('click', generateQuestaions, { signal: controller.signal });
+btn.addEventListener('click', generateQuestions, { once: true });
 
-// questions 정답 넣기
 
-// 문제 정답들을 나열한 json을 읽는다
-// 문제 정답 갯수만큼 questions 를 늘리고 정답을 넣어준다
 
-// 문제 마킹 목록 생성
-function generateQuestaions() {
-    for( var i = 0; i < questions.length; i++) {
+// 파일에서 정답 읽기
+// for (i = 0; i < data.question.length; i++) {
+//     questions.push({
+//         bShowAnswerImme: false,
+//         bShowCorrectImme: false,
+//         options: ["Option 1", "Option 2", "Option 3", "Option 4"], // 현재는 사용 안 함
+//         nCorrectAnswer: data.question[i].answer
+//     })
+// }
+
+// fetch('https://server.com/res/20220424.json')
+// .then((response) => response.json())
+// .then((json)=>console.log(json));
+
+// import data from '../res/20220424.json' assert { type: 'json' };
+// console.log(data);
+
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    };
+    rawFile.send(null);
+}
+
+readTextFile("milktone.github.io/res/20220424.json");
+
+// 답안체크 목록 생성
+function generateQuestions() {
+
+    for (var i = 0; i < questions.length; i++) {
         var question = questions[i];
         var questionElement = document.createElement("div");
-        questionElement.innerHTML = "<p>"+ i +"</p>";
+        questionElement.innerHTML = "<p>" + i + "</p>";
 
-        for (var j = 0; j<question.options.length; j++) {
+        for (var j = 0; j < question.options.length; j++) {
             var optionElement = document.createElement("input");
             optionElement.type = "radio";
             optionElement.name = "question_" + i;
@@ -50,8 +74,4 @@ function generateQuestaions() {
 
         markingContainer.appendChild(questionElement);
     }
-
-    controller.abort();
 }
-
-
